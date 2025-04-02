c421940bf863c386846d52a809d7cf002b6b5c50
Blrch0n <bolrochuluun2005@gmail.com>
2025-04-02 17:55:01 +0800

    init

---
+++ src/app/components/create-ads/CreateImage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateImage = () => {
+  return <div className="w-full border border-black">CreateImage</div>;
+};
+
+export default CreateImage;
--- src/app/vip-advertising/page.jsx
+++ src/app/vip-advertising/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import VipDetails from "../components/(vip-advertising)/VipDetail";
-import MainVipAds from "../components/(vip-advertising)/MainVipAds";
-import ProductIntro from "../components/(vip-advertising)/ProductIntro";
-import VipSection from "../components/(vip-advertising)/VipSection";
-import CommonQuestions from "../components/(vip-advertising)/CommonQuestions";
+import VipDetails from "../components/vip-advertising/VipDetail";
+import MainVipAds from "../components/vip-advertising/MainVipAds";
+import ProductIntro from "../components/vip-advertising/ProductIntro";
+import VipSection from "../components/vip-advertising/VipSection";
+import CommonQuestions from "../components/vip-advertising/CommonQuestions";
 
 const page = () => {
   return (
