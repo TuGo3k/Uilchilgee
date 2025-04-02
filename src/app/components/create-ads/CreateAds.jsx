ca24c8738b4c8c62c8ce452a4a99d546b3098df7
Blrch0n <bolrochuluun2005@gmail.com>
2025-04-02 18:50:51 +0800

    init

--- src/app/components/create-ads/CreateAds.jsx
+++ src/app/components/create-ads/CreateAds.jsx
@@ -1,12 +1,13 @@
+"use client";
 import React from "react";
 import CreateForm from "./CreateForm";
-import CreateImage from "./CreateImage";
+import MultiImageUpload from "./example";
 
 const CreateAds = () => {
   return (
     <div className="w-full h-fit gap-[43px] px-[10vw] gap flex flex-row">
       <CreateForm />
-      <CreateImage />
+      <MultiImageUpload />
     </div>
   );
 };
