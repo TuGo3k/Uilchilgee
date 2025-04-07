4c9b764870f4612a56084c60da5f3755c7c58441
Blrch0n <bolrochuluun2005@gmail.com>
2025-04-07 17:09:21 +0800

    init

---
+++ src/app/user-info/payment/page.jsx
@@ -0,0 +1,9 @@
+import UserSideBar from "../../components/user-info/UserSideBar";
+
+const page = () => {
+  <div className="w-full px-[20vw] py-[132px] gap-7 h-fit flex flex-row bg-white">
+    <UserSideBar />
+  </div>;
+};
+
+export default page;
